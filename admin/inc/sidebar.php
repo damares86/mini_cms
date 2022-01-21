ff85a204a99895304b6bc6b5a9d958734e45b443
davide <davide.masera@xstream-labs.com>
2022-01-21 17:27:03 +0100

    sidebar item definition

--- admin/inc/sidebar.php
+++ admin/inc/sidebar.php
@@ -7,20 +7,30 @@ if($_SESSION['user_id']==1){
                                 <li class="active">
                                     <a href="index.php"><i class="menu-icon icon-dashboard"></i>Dashboard</a>
                                 </li>
+                            </ul>
+                            <ul class="widget widget-menu unstyled">
                                 <li>
                                     <a href="index.php?man=users&op=edit&idToMod=1"><i class="menu-icon icon-user"></i>Profile</a>
                                 </li>
                                 <li>
-                                    <a href="index.php?man=post&op=show"><i class="menu-icon icon-edit"></i>Post</a>
-                                </li>
-                                <li>
                                     <a href="index.php?man=users&op=show"><i class="menu-icon icon-group"></i>Users</a>
                                 </li>
-                                <!-- <li>
-                                    <a href="index.php?man=roles&op=show"><i class="menu-icon icon-key"></i>Roles</a>
-                                </li> -->
+                            </ul>
+                            <ul class="widget widget-menu unstyled">
+
+                                <li>
+                                    <a href="index.php?man=post&op=show"><i class="menu-icon icon-edit"></i>Posts</a>
+                                </li>
+                                <li>
+                                    <a href="index.php?man=post&op=show"><i class="menu-icon icon-copy"></i>Pages</a>
+                                </li>
                                 <li>
                                     <a href="index.php?man=files&op=show"><i class="menu-icon icon-folder-open"></i>Files</a>
+                                </li>
+                            </ul>
+                            <ul class="widget widget-menu unstyled">
+                                <li>
+                                    <a href="index.php?man=post&op=show"><i class="menu-icon icon-wrench"></i>Settings</a>
                                 </li>
                             </ul>
                             <!--/.widget-nav-->
