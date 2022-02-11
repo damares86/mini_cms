bf0fee6ef7edd26b67bebfac4f4594afec0bc94b
davide <davide.masera@xstream-labs.com>
2022-02-11 17:37:54 +0100

    wip contact form

--- assets/dm_theme/inc/scripts.php
+++ assets/dm_theme/inc/scripts.php
@@ -1,11 +1,12 @@
-
+<!-- <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/checkout/"> -->
 		<script src="admin/assets/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 		<link rel="preconnect" href="https://fonts.googleapis.com">
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Baloo+Tammudu+2:wght@400;700&family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet">
 		<link type="text/css" href="admin/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<link type="text/css" href="admin/assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+		<!-- <link type="text/css" href="admin/assets/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet"> -->
 		<link href="assets/<?= $theme ?>/css/burger.css" rel="stylesheet" type="text/css">
+		<link href="assets/<?= $theme ?>/css/form-validation.css" rel="stylesheet" type="text/css">
 		<link href="assets/<?= $theme ?>/css/main.css" rel="stylesheet" type="text/css">
 		<link href="assets/<?= $theme ?>/css/custom.css" rel="stylesheet" type="text/css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
