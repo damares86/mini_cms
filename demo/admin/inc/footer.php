60c57b21befd3f6ddd79330d9f787a12033beecb
damares86 <85158984+damares86@users.noreply.github.com>
2022-04-04 15:52:30 +0200

    Delete demo directory

--- demo/admin/inc/footer.php
+++
@@ -1,9 +0,0 @@
-  <!-- Footer -->
-  <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Mini Cms is a project by <a href="https://www.davidemasera.it" target="_blank">davidemasera</a> - Theme by <a href="https://startbootstrap.com/" target="_blank">Start Bootstrap</a></span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
