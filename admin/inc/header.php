1fcddb0ddf38d204d69770ad037edca43ea39a0a
davide <davide.masera@xstream-labs.com>
2022-05-04 15:05:41 +0200

    added favicon

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -54,7 +54,8 @@ require "locale/$lang.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="author" content="damares86">
+    <link rel="icon" href="assets/img/favicon.ico" type="image/png"/>
 
     <title>Mini Cms - Dashboard</title>
 
