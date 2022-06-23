36386946c686ceb7f3d0927e97eda614324a41d2
davide <davide.masera@xstream-labs.com>
2022-06-23 17:34:53 +0200

    trying to save page data on json in order to avoid the limit of 6 blocks

--- admin/core/mngPage.php
+++ admin/core/mngPage.php
@@ -96,113 +96,129 @@ if(filter_input(INPUT_POST,"subReg")){
 	
 	if($operation=="add"){
 		//inserimento
-		$page->page_name=$_POST['page_name'];
-		$page->no_mod=0;
-		$page->layout=$_POST['layout'];
-		$page->theme=$_POST['theme'];
-		$page->img=$_FILES['myfile']['name'];
+
+		$arr=array(
+			"name"		=> $_POST['page_name'],
+			"no_mod"	=> 0,
+			"layout"	=> $_POST['layout'],
+			"theme"		=> $_POST['theme'],
+			"block1_type"=> 't',
+			"block1"	=> $_POST['editor1'],
+			"block1_bg"	=> $_POST['block1_bg'],
+			"block1_text"=>$_POST['block1_text']
+		);
+
+		$json=json_encode($arr);
+		$file="test.json";
+		file_put_contents($file, $json, FILE_APPEND);
+		exit;
+		// $page->page_name=$_POST['page_name'];
+		// $page->no_mod=0;
+		// $page->layout=$_POST['layout'];
+		// $page->theme=$_POST['theme'];
+		// $page->img=$_FILES['myfile']['name'];
 		
 		
-		if(isset($_POST['visualSel'])){
-			$page->header=$_POST['visualSel'];
-		} else {
-			$page->header=0;
-		}
+		// if(isset($_POST['visualSel'])){
+		// 	$page->header=$_POST['visualSel'];
+		// } else {
+		// 	$page->header=0;
+		// }
 		
-		$page->block1_type = "t";
-		$page->block1=$_POST['editor1'];
-		$page->block1_bg=$_POST['block1_bg'];
-		$page->block1_text=$_POST['block1_text'];
+		// $page->block1_type = "t";
+		// $page->block1=$_POST['editor1'];
+		// $page->block1_bg=$_POST['block1_bg'];
+		// $page->block1_text=$_POST['block1_text'];
 
-		if($_POST['block2'][0]=="g2"){
-			$gallery=$_POST['block2_gall'];
-			$gallery= str_replace(" ","_", $gallery);
-			$gallery=strtolower($gallery);
-			$page->block2_type=$gallery;	
-		}else if($_POST['block2'][0]=="t2"){
-			if($_POST['editor2']){
-				$page->block2=$_POST['editor2'];
-			}
-			$page->block2_type="t";
-		}else if($_POST['block2'][0]=="b2"){
-			$page->block2_type="b";
-		}else if($_POST['block2'][0]=="n2"){
-			$page->block2_type="n";
-		}
-		$page->block2_bg=$_POST['block2_bg'];
-		$page->block2_text=$_POST['block2_text'];
+		// if($_POST['block2'][0]=="g2"){
+		// 	$gallery=$_POST['block2_gall'];
+		// 	$gallery= str_replace(" ","_", $gallery);
+		// 	$gallery=strtolower($gallery);
+		// 	$page->block2_type=$gallery;	
+		// }else if($_POST['block2'][0]=="t2"){
+		// 	if($_POST['editor2']){
+		// 		$page->block2=$_POST['editor2'];
+		// 	}
+		// 	$page->block2_type="t";
+		// }else if($_POST['block2'][0]=="b2"){
+		// 	$page->block2_type="b";
+		// }else if($_POST['block2'][0]=="n2"){
+		// 	$page->block2_type="n";
+		// }
+		// $page->block2_bg=$_POST['block2_bg'];
+		// $page->block2_text=$_POST['block2_text'];
 
-		if($_POST['block3'][0]=="g3"){
-			$gallery=$_POST['block3_gall'];
-			$gallery= str_replace(" ","_", $gallery);
-			$gallery=strtolower($gallery);
-			$page->block3_type=$gallery;	
-		}else if($_POST['block3'][0]=="t3"){
-			if($_POST['editor3']){
-				$page->block3=$_POST['editor3'];
-			}
-			$page->block3_type="t";
-		}else if($_POST['block3'][0]=="b3"){
-			$page->block3_type="b";
-		}else if($_POST['block3'][0]=="n3"){
-			$page->block3_type="n";
-		}
-		$page->block3_bg=$_POST['block3_bg'];
-		$page->block3_text=$_POST['block3_text'];
+		// if($_POST['block3'][0]=="g3"){
+		// 	$gallery=$_POST['block3_gall'];
+		// 	$gallery= str_replace(" ","_", $gallery);
+		// 	$gallery=strtolower($gallery);
+		// 	$page->block3_type=$gallery;	
+		// }else if($_POST['block3'][0]=="t3"){
+		// 	if($_POST['editor3']){
+		// 		$page->block3=$_POST['editor3'];
+		// 	}
+		// 	$page->block3_type="t";
+		// }else if($_POST['block3'][0]=="b3"){
+		// 	$page->block3_type="b";
+		// }else if($_POST['block3'][0]=="n3"){
+		// 	$page->block3_type="n";
+		// }
+		// $page->block3_bg=$_POST['block3_bg'];
+		// $page->block3_text=$_POST['block3_text'];
 
-		if($_POST['block4'][0]=="g4"){
-			$gallery=$_POST['block4_gall'];
-			$gallery= str_replace(" ","_", $gallery);
-			$gallery=strtolower($gallery);
-			$page->block4_type=$gallery;	
-		}else if($_POST['block4'][0]=="t4"){
-			if($_POST['editor4']){
-				$page->block4=$_POST['editor4'];
-			}
-			$page->block4_type="t";
-		}else if($_POST['block4'][0]=="b4"){
-			$page->block4_type="b";
-		}else if($_POST['block4'][0]=="n4"){
-			$page->block4_type="n";
-		}
-		$page->block4_bg=$_POST['block4_bg'];
-		$page->block4_text=$_POST['block4_text'];
+		// if($_POST['block4'][0]=="g4"){
+		// 	$gallery=$_POST['block4_gall'];
+		// 	$gallery= str_replace(" ","_", $gallery);
+		// 	$gallery=strtolower($gallery);
+		// 	$page->block4_type=$gallery;	
+		// }else if($_POST['block4'][0]=="t4"){
+		// 	if($_POST['editor4']){
+		// 		$page->block4=$_POST['editor4'];
+		// 	}
+		// 	$page->block4_type="t";
+		// }else if($_POST['block4'][0]=="b4"){
+		// 	$page->block4_type="b";
+		// }else if($_POST['block4'][0]=="n4"){
+		// 	$page->block4_type="n";
+		// }
+		// $page->block4_bg=$_POST['block4_bg'];
+		// $page->block4_text=$_POST['block4_text'];
 
-		if($_POST['block5'][0]=="g5"){
-			$gallery=$_POST['block5_gall'];
-			$gallery= str_replace(" ","_", $gallery);
-			$gallery=strtolower($gallery);
-			$page->block5_type=$gallery;	
-		}else if($_POST['block5'][0]=="t5"){
-			if($_POST['editor5']){
-				$page->block5=$_POST['editor5'];
-			}
-			$page->block5_type="t";
-		}else if($_POST['block5'][0]=="b5"){
-			$page->block5_type="b";
-		}else if($_POST['block5'][0]=="n5"){
-			$page->block5_type="n";
-		}
-		$page->block5_bg=$_POST['block5_bg'];
-		$page->block5_text=$_POST['block5_text'];
+		// if($_POST['block5'][0]=="g5"){
+		// 	$gallery=$_POST['block5_gall'];
+		// 	$gallery= str_replace(" ","_", $gallery);
+		// 	$gallery=strtolower($gallery);
+		// 	$page->block5_type=$gallery;	
+		// }else if($_POST['block5'][0]=="t5"){
+		// 	if($_POST['editor5']){
+		// 		$page->block5=$_POST['editor5'];
+		// 	}
+		// 	$page->block5_type="t";
+		// }else if($_POST['block5'][0]=="b5"){
+		// 	$page->block5_type="b";
+		// }else if($_POST['block5'][0]=="n5"){
+		// 	$page->block5_type="n";
+		// }
+		// $page->block5_bg=$_POST['block5_bg'];
+		// $page->block5_text=$_POST['block5_text'];
 		
-		if($_POST['block6'][0]=="g6"){
-			$gallery=$_POST['block6_gall'];
-			$gallery= str_replace(" ","_", $gallery);
-			$gallery=strtolower($gallery);
-			$page->block6_type=$gallery;	
-		}else if($_POST['block6'][0]=="t6"){
-			if($_POST['editor6']){
-				$page->block6=$_POST['editor6'];
-			}
-			$page->block6_type="t";
-		}else if($_POST['block6'][0]=="b6"){
-			$page->block6_type="b";
-		}else if($_POST['block6'][0]=="n6"){
-			$page->block6_type="n";
-		}
-		$page->block6_bg=$_POST['block6_bg'];
-		$page->block6_text=$_POST['block6_text'];
+		// if($_POST['block6'][0]=="g6"){
+		// 	$gallery=$_POST['block6_gall'];
+		// 	$gallery= str_replace(" ","_", $gallery);
+		// 	$gallery=strtolower($gallery);
+		// 	$page->block6_type=$gallery;	
+		// }else if($_POST['block6'][0]=="t6"){
+		// 	if($_POST['editor6']){
+		// 		$page->block6=$_POST['editor6'];
+		// 	}
+		// 	$page->block6_type="t";
+		// }else if($_POST['block6'][0]=="b6"){
+		// 	$page->block6_type="b";
+		// }else if($_POST['block6'][0]=="n6"){
+		// 	$page->block6_type="n";
+		// }
+		// $page->block6_bg=$_POST['block6_bg'];
+		// $page->block6_text=$_POST['block6_text'];
 
 		
 		
