596ecc269e0ca37404301c9c29b4f083389bf28a
damares <davide.masera@xstream-labs.com>
2022-09-22 17:12:01 +0200

    code cleaning

--- admin/template/inc/header.php
+++ admin/template/inc/header.php
@@ -1,11 +1,11 @@
 <?php
 
-require 'admin/phpDebug/src/Debug/Debug.php';   			// if not using composer
+// require 'admin/phpDebug/src/Debug/Debug.php';   			// if not using composer
 
-$debug = new \bdk\Debug(array(
-    'collect' => true,
-    'output' => true,
-));
+// $debug = new \bdk\Debug(array(
+//     'collect' => true,
+//     'output' => true,
+// ));
 
 session_start();
 // loading class
