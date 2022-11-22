e8ce09508eaa27e442011e3964ba02fa52cbea8c
davide <davide.masera@xstream-labs.com>
2022-11-22 12:15:25 +0100

    template cleaning

--- assets/damares/inc/footerScript.php
+++
@@ -1,7 +0,0 @@
-
-
-			<script src="assets/<?=$theme?>/js/jquery.dropotron.min.js"></script>
-			<script src="assets/<?=$theme?>/js/browser.min.js"></script>
-			<script src="assets/<?=$theme?>/js/breakpoints.min.js"></script>
-			<script src="assets/<?=$theme?>/js/util.js"></script>
-			<script src="assets/<?=$theme?>/js/main.js"></script>
