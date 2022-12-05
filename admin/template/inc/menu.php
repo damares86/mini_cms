7146085cc390d2a53061b4b29b81d85bbb32079a
davide <davide.masera@xstream-labs.com>
2022-12-05 17:30:32 +0100

    fixed onepager behaviour, changed master template, contact and login need to be added as json files

--- admin/template/inc/menu.php
+++ admin/template/inc/menu.php
@@ -1,17 +1,24 @@
 <ul>
     <?php
+    $page_order=[];
+    $link="";
+    $link_child="";
 
     $stmt=$menu->showAllParent();
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
         extract($row);
-        // $name=$pagename;
         $class="";
         $name=$row['pagename'];
         $str=$row['pagename'];
         $str = preg_replace('/\s+/', '_', $str);
         $str = strtolower($str);
+        $page_order[]=$str;
+        $link=$root.$str.".php";
+        if($one){
+            $link="#$str";
+        }
         ?>
-        <li><a href="<?=$root?><?=$str?>.php" ><?php
+        <li><a href="<?=$link?>" class="scrolly"><?php
         if($name=='index'){
             echo "Home";
         } else if($name=="Post"||$name=="Blog"){
@@ -39,8 +46,13 @@
                         }
                         $str1 = preg_replace('/\s+/', '_', $str1);
                         $str1 = strtolower($str1);
+                        $page_order[]=$str1;
+                        $link_child=$root.$str1.".php";
+                        if($one){
+                            $link_child="#$str1";
+                        }
                         ?>
-                            <li style="white-space: nowrap;"><a href="<?=$root?><?=$str1?>.php" style="display: block;"><?php
+                            <li style="white-space: nowrap;"><a href="<?=$link_child?>" style="display: block;" class="scrolly"><?php
                                 if($row1['pagename']=='index'){
                                     echo "Home";
                                 } else {
@@ -58,7 +70,6 @@
         </li>
         <?php
         }
-    
     ?>
 
 </ul>
